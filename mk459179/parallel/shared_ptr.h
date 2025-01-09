1835f9aeae0cca906332e89c3020e363abbe50ea
maciej.konert <maciej@grekomp.com>
2025-01-09 19:04:35 +0100

    Added basic functions for parallel.

---
+++ mk459179/parallel/shared_ptr.h
@@ -0,0 +1,8 @@
+//
+// Created by maciej on 09.01.25.
+//
+
+#ifndef MK459179_SHARED_PTR_H
+#define MK459179_SHARED_PTR_H
+
+#endif //MK459179_SHARED_PTR_H
---
+++ mk459179/parallel/stack.h
@@ -0,0 +1,8 @@
+//
+// Created by maciej on 09.01.25.
+//
+
+#ifndef MK459179_STACK_H
+#define MK459179_STACK_H
+
+#endif //MK459179_STACK_H
